9c3ee9c2533a8fd415e19ab252f7c4e908489b5e
Christopher Peret <cperet89@gmail.com>
2023-03-27 18:25:36 -0700

    script.js generate function while loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,8 +14,8 @@ function generatePassword() {
     // isNaN used to check (edge case) if input is a number
     console.log(passwordLength);
 
-  while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
-    passwordLength = parseInt(prompt("Invalid input. Please enter password length (8-128 characters):"));
+    while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+        passwordLength = parseInt(prompt("Invalid input. Please enter password length (8-128 characters):"));
     }
    
     // Prompt user for character types
@@ -24,5 +24,16 @@ function generatePassword() {
     let includeNumbers = confirm("Click OK to confirm including numeric characters");
     let includeSpecial = confirm("Click OK to confirm including special characters");
 
-    
+    // while loop to validate at least one character type
+    // if all character types are false, alert user and re-prompt
+    // if any character type is true, break out of loop
+    // ! used to check if all character types are false, requires truthy value
+    while (!(includeLowercase || includeUppercase || includeNumbers || includeSpecialChars)) {
+        alert("Please select at least one character type.");
+        includeLowercase = confirm("Include lowercase characters?");
+        includeUppercase = confirm("Include uppercase characters?");
+        includeNumbers = confirm("Include numeric characters?");
+        includeSpecial = confirm("Include special characters?");
+    }
+
 }
