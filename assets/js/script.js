d2d8ca378c7dbbe56bde513a7b7916af84bead51
Christopher Peret <cperet89@gmail.com>
2023-03-27 18:21:48 -0700

    script.js generate function started

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,15 @@ var generateBtn = document.querySelector("#generate");
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//initial Password Generator Function
+function generatePassword() {
+
+    // Prompt user for password length
+    // parseint used to convert string to integer
+    let passwordLength = parseInt(prompt("How many characters would you like your password to contain? (Must be 8-128) "));
+    // if statement to check if password length is between 8 and 128
+    // isNaN used to check (edge case) if input is a number
+    console.log(passwordLength);
+
+}
